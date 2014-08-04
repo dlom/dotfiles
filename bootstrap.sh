96937015ca64c6fcfc94bd6a1be4cdd4463445c7
Mark <dlom234@gmail.com>
2014-08-04 15:33:36 -0700

    bootstrap our dotfiles into /home/mark

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+function dotfiles() {
+	rsync --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE" --archive --verbose --human-readable "$@" ./ $HOME/
+}
+
+echo "--> Getting the hottest dotfiles, right off the press"
+git pull origin master
+
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
+	echo "--> You forced my hand, man"
+	dotfiles
+else
+	echo "--> Here's what's gonna happen: "
+	dotfiles --dry-run
+	read "REPLY?--> You really wanna do this? "
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		dotfiles
+		echo "--> Job's done"
+	else
+		echo "--> Nothing happened!"
+	fi
+fi

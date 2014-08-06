3cb27a499843da745271f814790a8990ee24664a
Mark <dlom234@gmail.com>
2014-08-05 20:02:31 -0700

    better bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,17 @@ function dotfiles() {
 	rsync --exclude ".git/" --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE" --no-perms --archive --verbose --human-readable "$@" ./ $HOME/
 }
 
-echo "--> Getting the hottest dotfiles, right off the press"
-git pull origin master
-
-if [[ "$1" == "--force" || "$1" == "-f" ]]; then
-	echo "--> You forced my hand, man"
+if [[ "$1" == "--yes" || "$1" == "-y" ]]; then
+	echo "--> The computer is always smarter than the human..."
+	git pull origin master
 	dotfiles
 else
-	echo "--> Here's what's gonna happen: "
+	read "REPLY?--> Do you want the latest, greatest dotfiles? "
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "--> Getting the hottest dotfiles, right off the press"
+		git pull origin master
+	fi
+	echo "--> Okay, here's what's gonna happen: "
 	dotfiles --dry-run
 	read "REPLY?--> You really wanna do this? "
 	if [[ $REPLY =~ ^[Yy]$ ]]; then

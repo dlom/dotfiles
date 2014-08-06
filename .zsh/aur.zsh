ec260e594b0ac2c161d13d73939d8ae02ab5b754
Mark <dlom234@gmail.com>
2014-08-05 19:33:09 -0700

    new aur helper

--- .zsh/aur.zsh
+++ .zsh/aur.zsh
@@ -1,12 +1,19 @@
 # aur helpers
+# installs all arguments (no aur dep checking)
 function auri() {
 	pushd -q `mktemp -d`
 	bash <(curl aur.sh --silent) --syncdeps --install "$@"
 	popd -q
 }
 
+# updates ALL aur packages
 function auru() {
 	pushd -q `mktemp -d`
 	pacman --query --quiet --foreign | xargs bash <(curl aur.sh --silent) --syncdeps --install --needed
 	popd -q
+}
+
+# prints out aur packages that need updating
+function aurc() {
+	package-query --aur --foreign --format "%n %v" | comm - <(package-query --aur --foreign --format "%n %V") -23 | awk '{print $1}'
 }
